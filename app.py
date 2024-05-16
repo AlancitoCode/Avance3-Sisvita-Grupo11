abbffe02093de847900ebd5ee49ce9bd6fffb9c0
AlancitoCode <alancarbajalr5@gmail.com>
2024-05-16 01:09:17 -0500

    Guardado

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from utils.db import db
-from services.predio import predios
+from services.estudiante import estudiantes
+from services.test import tests
+from services.area import areas
+from services.pregunta import preguntas
+from services.respuesta import respuestas
 from flask_sqlalchemy import SQLAlchemy
+from services.predio import predios
 from config import DATABASE_CONNECTION
 
 app=Flask(__name__)
@@ -10,7 +15,11 @@ app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_CONNECTION
 #SQLAlchemy(app)
 
 db.init_app(app) 
-app.register_blueprint(predios)
+app.register_blueprint(estudiantes)
+app.register_blueprint(tests)
+app.register_blueprint(areas)
+app.register_blueprint(preguntas)
+app.register_blueprint(respuestas)
 
 with app.app_context():
     db.create_all
